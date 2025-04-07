cbf0125392ab433b5bc9e4e01782a4aca7e49e47
svetlana <svetlana.kalva.ic@gmail.com>
2025-04-07 20:19:00 +0300

    [master]: Learn Java Variables

---
+++ src/main/java/v/acodemy/MyClass.java
@@ -0,0 +1,69 @@
+package v.acodemy;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Ja u Mami programmistka!");
+
+        // byte 8-bit
+           byte age = 30;
+        System.out.println(age);
+
+        //Naming conventio:
+        // Camel Case: MyClass
+        // Camel Case: currentYear for
+
+        //short 16-bit
+
+        short currentYear = 2025;
+        short temperature = -373;
+        short distance = 1600;
+
+        // int 32-bit
+        int score = 9000;
+        int population = 14440000;
+        int result = (10 * 10) -42;
+        System.out.println(result);
+
+        // long 64-bit
+        long starInGalaxy = 100_000_000_000L; // 100000000000
+        long bigNumber = 8979866788L;
+        long currentMillis = System.currentTimeMillis();
+      //  System.out.println(currentTimeMillis);
+
+        //float 32-bit
+        float pi = 3.14f;
+        float piNumber = (float) Math.PI; //casting
+        float discount = 25.5f;
+
+        // double 64-bit
+        double accountBalance = 9999.9999997877;
+        double squareRoot = Math.sqrt(2);
+        System.out.println(squareRoot);
+
+        // char (symbol)
+        char initial ='N';
+        char digit = '7';
+        char symbol = '#';
+
+
+        // See unicode table https:symbl.cc/en/unicode-table/
+
+
+
+        //boolean (F/T)
+        boolean osJavaFun = true;
+
+        // String
+
+        String myFullName = "Svetlana K"; // immutable
+        String mfn = new String();
+
+
+
+
+
+
+
+
+    }
+}
