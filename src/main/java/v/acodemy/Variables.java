340458a22073d3125058acf43a8f1c127118d98b
svetlana <svetlana.kalva.ic@gmail.com>
2025-04-17 17:43:10 +0300

    Array and Loop

---
+++ src/main/java/v/acodemy/Variables.java
@@ -0,0 +1,86 @@
+package v.acodemy;
+
+// import java.util.Arrays;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("123 " );
+
+        // Examples for "byte"
+        byte numPets = 2;
+        System.out.println(numPets);
+
+        byte shoesSize = 49;
+        System.out.println(shoesSize);
+        byte hairLength = 77;
+        System.out.println(hairLength);
+        byte sleepHours = 8;
+        System.out.println(sleepHours);
+        byte numLanguages = 5;
+        System.out.println(numLanguages);
+        byte numFingers = 20;
+        System.out.println(numFingers);
+
+        // Examples for "short"
+       short weightKg = 65;
+        System.out.println(weightKg);
+       short dailyCalories = 5897;
+        System.out.println(dailyCalories);
+       short heightCm = 195 ;
+        System.out.println(heightCm);
+       short apartmentNum = 100/2 ;
+        System.out.println(apartmentNum);
+       short apartmentFlor = -1 ;
+        System.out.println(apartmentFlor);
+       short birthYear =2000 ;
+        System.out.println(birthYear);
+
+        // Examples for "int"
+        int personID = 363654587;
+        System.out.println(personID);
+        int insuranceNum = 677676776;
+        System.out.println(insuranceNum);
+        int libraryNum = 123123;
+        System.out.println(libraryNum);
+        int booksReadNum = 2000;
+        System.out.println(booksReadNum);
+        int houseNum = 101;
+        System.out.println(houseNum);
+
+        // Examples for "long"
+        long passportNum = 1223456789;
+        System.out.println(passportNum);
+        long studentID = 1212121212;
+        System.out.println(studentID);
+        long drivingNumber = 20000002;
+        System.out.println(drivingNumber);
+        long insurancePolicyNum = 202020202;
+        System.out.println(insurancePolicyNum);
+        long creditCardNum = 2010101000;
+        System.out.println(creditCardNum);
+
+        // Examples for "float"
+
+        float temperature = 37.5f;
+        System.out.println(temperature);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
